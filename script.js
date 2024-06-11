92f719c1d2ecb70daebe6eb163368ded01a5b558
randyaf <randyaf29@gmail.com>
2024-06-11 15:43:35 +0700

    Refactor inputNumber function to make the code more concise
    - added new functions for adding math operator to the variable
      mainOperatorInput  but, for now only addAdditionOperation() that works,
      others are just empty function for now.
    - with the refactored inputNumber() now we can safely input dot/point
      before and after math operator

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operatorQueue = "";
 let lastDigitInput = "";
-let inputtedNumber = [];
+let mainOperationInput = [];
 
 // basic operation
 
@@ -44,34 +44,113 @@ function processNumber(event) {
 function inputNumber(event) {
     if (event.target.matches(".number")) {
         const number = event.target.getAttribute("data-value");
-        console.log("test" + number);
-        // add number to array
-        if (inputtedNumber.length === 0) {
-            if (number === ".") {
-                inputtedNumber.push("0");
-                inputtedNumber.push(".");
-            } else {
-                inputtedNumber.push(number);
-            }
-        } else if (number === "." && !inputtedNumber.includes(".")) {
-            console.log("if 2");
-            inputtedNumber.push(number);
-        } else if (inputtedNumber.length === 1 && inputtedNumber[0] === "0") {
-            if(number !== "0" && number !== ".") {
-                inputtedNumber.pop();
-                inputtedNumber.push(number);
-            }
-            console.log("if 3")
-        } else if (number !== "."){
-            inputtedNumber.push(number);
-            console.log("if 4");
+
+        if (mainOperationInput.some(item => {
+            return (
+            item === "+" ||
+            item === "-" ||
+            item === "*" ||
+            item === "/")
+        })){
+            inputAfterOperator(number);
+        } else {
+            inputBeforeOperator(number);
         }
     }
-    console.log(inputtedNumber);
+
+    console.log(mainOperationInput);
+}
+
+function inputBeforeOperator(number) {
+
+    if (mainOperationInput.length === 0) {
+        if (number === ".") {
+            mainOperationInput.push("0");
+            mainOperationInput.push(".");
+        } else {
+            mainOperationInput.push(number);
+        }
+    } else if (number === "." && !mainOperationInput.includes(".")) {
+        console.log("if 2");
+        mainOperationInput.push(number);
+    } else if (mainOperationInput.length === 1 && mainOperationInput[0] === "0") {
+        if(number !== "0" && number !== ".") {
+            mainOperationInput.pop();
+            mainOperationInput.push(number);
+        }
+        console.log("if 3")
+    } else if (number !== "."){
+        mainOperationInput.push(number);
+        console.log("if 4");
+    }
+}
+
+function inputAfterOperator(number) {
+    const operatorIndex = mainOperationInput.findIndex(item => {
+                        return (item === "+" ||
+                                item === "-" ||
+                                item === "*" ||
+                                item === "/");
+                    });
+    const rightHandNumber = [...mainOperationInput].slice(operatorIndex+1);
+
+    console.log("operator-index: " + operatorIndex);
+    console.log("right-value: " + rightHandNumber);
+    console.log("right-length: " + rightHandNumber.length);
+
+    if (rightHandNumber.length === 0) {
+        if (number === ".") {
+            mainOperationInput.push("0");
+            mainOperationInput.push(".");
+        } else {
+            mainOperationInput.push(number);
+        }
+    } else if (number === "." && !rightHandNumber.includes(".")) {
+        console.log("after if 2");
+        mainOperationInput.push(number);
+    } else if (rightHandNumber.length === 1 && rightHandNumber[0] === "0") {
+        if(number !== "0" && number !== ".") {
+            mainOperationInput.pop();
+            mainOperationInput.push(number);
+        }
+        console.log("after if 3")
+    } else if (number !== "."){
+        mainOperationInput.push(number);
+        console.log("after if 4");
+    }
 }
 
 function processOperator(event) {
+    const operator = event.target.matches(".operator-button") ? event.target : null;
+    const operatorValue = operator.getAttribute("data-value");
 
+    if (operatorValue === "+") addAdditionOperator(event);
+    else if (operatorValue === "-") addSubtractionOperator(event);
+    else if (operatorValue === "*") addMultiplicationOperator(event);
+    else if (operatorValue === "/") addDivisionOperator(event);
+    else if (operatorValue === "=") evaluateOperation(event);
+}
+
+function addAdditionOperator(event) {
+    operatorQueue = "+";
+    mainOperationInput.push(operatorQueue);
+    console.log("addition");
+}
+
+function addSubtractionOperator(event) {
+    console.log("subtraction");
+}
+
+function addMultiplicationOperator(event) {
+    console.log("multiplication");
+}
+
+function addDivisionOperator(event) {
+    console.log("division");
+}
+
+function evaluateOperation(event) {
+    console.log("evaluation");
 }
 
 function processSecondaryOperator(event) {
