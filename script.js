c6969218bd62d0e9c246e62a7dc29c41326c324f
randyaf <randyaf29@gmail.com>
2024-06-11 08:53:01 +0700

    Add listeners to all necessary buttons to function
    - the functions related to the listners are not implemented yet
      so I'll write them later on

--- script.js
+++ script.js
@@ -21,3 +21,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// add listener to all necessary buttons
+
+const numbersContainer = document.querySelector(".numbers-container");
+numbersContainer.addEventListener("click", processNumber);
+
+const mainOperatorContainer = document.querySelector(".bottom-right-container");
+mainOperatorContainer.addEventListener("click", processOperator);
+
+const secondaryOperatorContainer = document.querySelector(".secondary-operator-container");
+secondaryOperatorContainer.addEventListener("click", processSecondaryOperator);
+
+const deleteButton = document.querySelector(".delete-button");
+deleteButton.addEventListener("click", deleteDisplay);
