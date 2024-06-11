e9e747e4d0d30bd85fb640dada6f460ef93bf5db
randyaf <randyaf29@gmail.com>
2024-06-11 10:42:56 +0700

    Create inputNumber function to safely save to array
    - also added data-value attribute for each element of
      numbers buton.
    - added empty function for every event listner in order
      for the to work

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let firstNumber = 0;
 let secondNumber = 0;
 let operatorQueue = "";
 let lastDigitInput = "";
-let mainOperation = "";
+let inputtedNumber = [];
 
 // basic operation
 
@@ -35,3 +35,49 @@ secondaryOperatorContainer.addEventListener("click", processSecondaryOperator);
 
 const deleteButton = document.querySelector(".delete-button");
 deleteButton.addEventListener("click", deleteDisplay);
+
+function processNumber(event) {
+    inputNumber(event);
+
+}
+
+function inputNumber(event) {
+    if (event.target.matches(".number")) {
+        const number = event.target.getAttribute("data-value");
+        console.log("test" + number);
+        // add number to array
+        if (inputtedNumber.length === 0) {
+            if (number === ".") {
+                inputtedNumber.push("0");
+                inputtedNumber.push(".");
+            } else {
+                inputtedNumber.push(number);
+            }
+        } else if (number === "." && !inputtedNumber.includes(".")) {
+            console.log("if 2");
+            inputtedNumber.push(number);
+        } else if (inputtedNumber.length === 1 && inputtedNumber[0] === "0") {
+            if(number !== "0" && number !== ".") {
+                inputtedNumber.pop();
+                inputtedNumber.push(number);
+            }
+            console.log("if 3")
+        } else if (number !== "."){
+            inputtedNumber.push(number);
+            console.log("if 4");
+        }
+    }
+    console.log(inputtedNumber);
+}
+
+function processOperator(event) {
+
+}
+
+function processSecondaryOperator(event) {
+
+}
+
+function deleteDisplay(event) {
+
+}
