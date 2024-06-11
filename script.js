6f606de7c033666986c8454f75873af181c661c7
randyaf <randyaf29@gmail.com>
2024-06-11 08:41:45 +0700

    Add basic math operation functions

---
+++ script.js
@@ -0,0 +1,23 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operatorQueue = "";
+let lastDigitInput = "";
+let mainOperation = "";
+
+// basic operation
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
